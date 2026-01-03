89ddf49d29d8d84fd0222b0855fe95267c3c1770
marimoisreal <lolkeklol626@gmail.com>
2026-01-03 20:48:48 +0000

    added google icons

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -143,9 +143,9 @@
                 <div class="input-group input-group-lg shadow">
                     <input type="text" name="search" class="form-control search-input" 
                            placeholder="Search members..." value="{{ request('search') }}">
-                    <button class="btn btn-primary px-5" type="submit">Search</button>
+                    <button class="btn btn-primary px-5" type="submit"><span class="material-symbols-outlined">search</span>Search</button>
                     @if(request('search'))
-                        <a href="{{ route('admin.users.index') }}" class="btn btn-secondary d-flex align-items-center">Reset</a>
+                        <a href="{{ route('admin.users.index') }}" class="btn btn-secondary d-flex align-items-center"><span class="material-symbols-outlined">cancel</span>Reset</a>
                     @endif
                 </div>
             </form>
